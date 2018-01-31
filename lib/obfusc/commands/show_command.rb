1853cc12e0c4eab7d6a4a87212fa01c6282e959d
Marcos G. Zimmermann <mgzmaster@gmail.com>
2018-01-30 23:08:01 -0200

    Add show command

---
+++ lib/obfusc/commands/show_command.rb
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+module Obfusc
+  # Perform tasks related `show` command
+  class ShowCommand
+
+    def initialize(config, source)
+      @config = config
+      @source = source
+    end
+
+    def self.call(config, *args)
+      source = args.first
+      model = new(config, source)
+      model.public_send(File.exist?(source) ? :run : :show_usage)
+    end
+
+    def run
+      files.each do |from, to|
+        puts "#{from}:"
+        puts "---> #{to}"
+      end
+    end
+
+    def show_usage
+      usage = <<-TEXT.gsub('      ', '')
+      Usage:
+      $ obfusc show <source>
+
+      Files:
+        source: Relative or absolute directory where obfuscated files are stored. (Default to current directory)
+      TEXT
+      puts usage
+    end
+
+    protected
+
+    def files
+      recursive_from = File.join(@source, '**/{.*,*}') if File.directory?(@source)
+      Dir.glob(recursive_from || @source).each_with_object({}) do |path, memo|
+        next if File.directory?(path)
+        next if File.symlink?(path)
+        basename = File.basename(path)
+        next unless @config.encryptor.obfuscated?(basename)
+        memo[path] = @config.encryptor.decrypt(basename)
+      end
+    end
+  end
+end
